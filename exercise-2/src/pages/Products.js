358a52d7434c1ac1e9b64d798d5de3495bb5fac7
Yuxuan <nine.slide@gmail.com>
2022-10-26 18:05:25 +0200

    programatically navigate

--- exercise-2/src/pages/Products.js
+++ exercise-2/src/pages/Products.js
@@ -1,6 +1,11 @@
-import { Link } from 'react-router-dom';
-
+import { Link ,useNavigate} from 'react-router-dom';
+//' no more useHistory, 
 const Products = () => {
+  const navigate = useNavigate()
+  navigate('/welcome',{replace: true})
+  //'if want to redirect, use replace:true, which will replace current route with new one instead of pushing
+  navigate(-1)
+  //' use numbers -1 go back one page, 1 go forward one page n for n pages
   return (
     <section>
       <h1>The Products Page</h1>
