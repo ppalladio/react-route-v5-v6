c9013ad7cd456cf2ce9a2d837bd32dfa07b898b6
Yuxuan <nine.slide@gmail.com>
2022-10-26 17:44:53 +0200

    nested route

--- exercise-2/src/App.js
+++ exercise-2/src/App.js
@@ -1,32 +1,28 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-
+import { Route, Routes, Navigate } from 'react-router-dom';
+//' redirect replaced ny navigate
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
 import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
-  return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/' exact>
-            <Redirect to='/welcome' />
-          </Route>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Routes>
+                    <Route path="/" element={<Navigate replace to="/welcome" />} />
+                    {/* //' without replace keyword we would be pushing the new page. */}
+                    <Route path="/welcome/*" element={<Welcome />} />
+                    <Route path="/products"  element={<Products />} />
+                    <Route
+                        path="/products/:productId"
+                        element={<ProductDetail />}
+                    />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- exercise-2/src/pages/Welcome.js
+++ exercise-2/src/pages/Welcome.js
@@ -1,14 +1,15 @@
-import { Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 const Welcome = () => {
-  return (
-    <section>
-      <h1>The Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
-    </section>
-  );
+    return (
+        <section>
+            <h1>The Welcome Page</h1>
+            <Routes>
+                <Route path="new-user" element={ <p>Welcome, new user!</p>}/>
+         {/* //'every route has to be wrapped with routes, and for the path, there is no need to repeat the parent path */}
+            </Routes>
+        </section>
+    );
 };
 
 export default Welcome;
